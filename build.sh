e00b458874d9ce5c47be3d9bcc6692bc8430e1f2
Norberto L. Herz <Nohorbee@Norbertos-MacBook-Pro.local>
2016-10-25 21:05:18 -0300

    new jekyll 3 w translated dates

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 git checkout master &&
 mkdir dontdelete  &&
 cp -r _site dontdelete/_site &&
-cp index.html dontdelete/index.html &&
+cp CNAME dontdelete/CNAME &&
 
 find . -maxdepth 1 ! -name 'dontdelete' ! -name '.*' | xargs rm -rf &&
 cp -r dontdelete/_site/* . &&
-cp -r dontdelete/index.html .
+cp -r dontdelete/CNAME .
 
 rm -rf dontdelete
 
